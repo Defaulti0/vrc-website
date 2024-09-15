c008d930598524cb30a74b20ba577feaecf958c6
Defaulti0 <crismartin994@gmail.com>
2024-09-15 16:54:14 -0400

    Implemented page routing

---
+++ src/pages/games/index.js
@@ -0,0 +1,12 @@
+import { Button } from "@material-tailwind/react";
+
+export default function Games() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
