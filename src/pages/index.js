031a6a474e2009118abeb79af95586bceb9add89
Defaulti0 <crismartin994@gmail.com>
2024-09-28 15:22:40 -0400

    checking for page router

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,20 @@
-import { Button } from '@material-tailwind/react'
+import { Button } from '@material-tailwind/react/components/Button'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
+import { UseIsAppRouter } from '@/useIsAppRouter'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div
-      // className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-      className="flex w-max gap-4"
+      className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
+      // className="flex w-max gap-4"
     >
+      <UseIsAppRouter />
       <p>Hello world!</p>
-      <Button>Text</Button>
-
-      <Link href="/games"></Link>
+      {/* <Button>Text</Button> */}
+      <Link href="/games">Games</Link>
     </div>
   )
 }
